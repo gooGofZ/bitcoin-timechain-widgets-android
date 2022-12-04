1f7ef43fdde35b0877af54e4062fdca27bdc6a3a
gooGofZ <darksmartz@gmail.com>
2022-12-04 08:52:07 +0700

    Revert "Add new data providers bitnodes.io, bitcoinexplorer.com"

--- app/src/main/java/com/googof/bitcointimechainwidgets/MempoolAppWidget.kt
+++ app/src/main/java/com/googof/bitcointimechainwidgets/MempoolAppWidget.kt
@@ -8,26 +8,30 @@ import android.content.Intent
 import android.os.Build
 import android.widget.RemoteViews
 import androidx.annotation.RequiresApi
-import com.googof.bitcointimechainwidgets.network.BitnodesApi
+import com.googof.bitcointimechainwidgets.network.MempoolApi
 import kotlinx.coroutines.*
-import java.text.SimpleDateFormat
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import java.util.*
 
 /**
  * Implementation of App Widget functionality.
  */
-class BitnodesAppWidget : AppWidgetProvider() {
+class MempoolAppWidget : AppWidgetProvider() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
+        // There may be multiple widgets active, so update all of them
+        //val scope = CoroutineScope(newSingleThreadContext("name"))
+
         for (appWidgetId in appWidgetIds) {
+            // GlobalScope.launch { // Delicate API
+            // CoroutineScope(Dispatchers.IO).launch { // Work at add and manual refresh
+            // GlobalScope.launch(Dispatchers.Main) { // Work at add and manual refresh // Delicate API
             CoroutineScope(Dispatchers.Main).launch {
-                updateBitnodesAppWidget(context, appWidgetManager, appWidgetId)
+                updateAppWidget(context, appWidgetManager, appWidgetId)
             }
         }
     }
@@ -41,40 +45,45 @@ class BitnodesAppWidget : AppWidgetProvider() {
     }
 }
 
-internal fun convertEpochtoDateTimeString(epoch: Int): String {
-    val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm")
-    val netDate = Date(epoch.toLong() * 1000)
-    return sdf.format(netDate)
-}
-
-
 @RequiresApi(Build.VERSION_CODES.O)
-internal suspend fun updateBitnodesAppWidget(
+internal suspend fun updateAppWidget(
     context: Context,
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int
 ) {
     // Call REST API
-    val response = BitnodesApi.retrofitService.getTotalNodes()
-
-    // Set response to object
-    val snapshotTime = convertEpochtoDateTimeString(response.results[0].timestamp)
-    val totalNodes = response.results[0].total_nodes
+    val blockHeight = MempoolApi.retrofitService.getBlockTipHeight()
+    val fees = MempoolApi.retrofitService.getRecommendedFee()
+    val hashRate =  MempoolApi.retrofitService.getHashrate()
+    val unconfirmedTX = MempoolApi.retrofitService.getUncomfirmedTX()
 
     // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.total_nodes_app_widget)
+    val views = RemoteViews(context.packageName, R.layout.mempool_app_widget)
 
-    // Set values to widget
-    views.setTextViewText(R.id.textSnapshotTime, snapshotTime)
-    views.setTextViewText(R.id.textTotalNodes, "%,d".format(totalNodes))
+    // Set Block Details
+    views.setTextViewText(R.id.textBlockHeight, blockHeight)
+
+    // Set Transaction Fees
+    views.setTextViewText(R.id.textHighPriority, fees.fastestFee)
+    views.setTextViewText(R.id.textHalfHourFee, fees.halfHourFee)
+    views.setTextViewText(R.id.textHourFee, fees.hourFee)
+
+    // Set Hashrate and difficulty EH/s
+    val currentHashrate = hashRate.currentHashrate / (1000000000000000000) //EH/s
+    val currentHashrateText = currentHashrate.toInt().toString() + " " + "EH/s"
+    views.setTextViewText(R.id.textHashRate, currentHashrateText)
 
     // Set last update date time
     val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
     val current = LocalDateTime.now().format(formatter)
     views.setTextViewText(R.id.textLastUpdated, current)
 
+    // Set Unconfirmed Transactions
+    val unconfirmedTXCount = unconfirmedTX.count
+    views.setTextViewText(R.id.textUnconfirmedTX, "%,d".format(unconfirmedTXCount) + " TXs")
+
     // refresh button
-    val intentUpdate = Intent(context, BitnodesAppWidget::class.java)
+    val intentUpdate = Intent(context, MempoolAppWidget::class.java)
     intentUpdate.action = AppWidgetManager.ACTION_APPWIDGET_UPDATE
 
     val idArray = intArrayOf(appWidgetId)
