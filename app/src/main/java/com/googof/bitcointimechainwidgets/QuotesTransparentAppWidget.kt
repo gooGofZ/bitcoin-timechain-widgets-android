475bd757fe56592b6938f8dd4e1a92d6291a5dda
Sarawut R <sarawut.r@dit.daikin.co.jp>
2022-12-05 01:12:08 +0700

    Add new data providers bitnodes.io, bitcoinexplorer.org Add new widgets from bitcoinexplorer.org (Bitcoin related quotes with background and without background) Add new widget from bitnodes.io (Total bitcoin nodes)

--- app/src/main/java/com/googof/bitcointimechainwidgets/QuotesTransparentAppWidget.kt
+++ app/src/main/java/com/googof/bitcointimechainwidgets/QuotesTransparentAppWidget.kt
@@ -10,29 +10,21 @@ import android.widget.RemoteViews
 import androidx.annotation.RequiresApi
 import com.googof.bitcointimechainwidgets.network.BitcoinExplorerApi
 import kotlinx.coroutines.*
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 import java.util.*
 
 /**
  * Implementation of App Widget functionality.
  */
-class BitcoinExplorerAppWidget : AppWidgetProvider() {
+class QuotesTransparentAppWidget : AppWidgetProvider() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
-        // There may be multiple widgets active, so update all of them
-        //val scope = CoroutineScope(newSingleThreadContext("name"))
-
         for (appWidgetId in appWidgetIds) {
-            // GlobalScope.launch { // Delicate API
-            // CoroutineScope(Dispatchers.IO).launch { // Work at add and manual refresh
-            // GlobalScope.launch(Dispatchers.Main) { // Work at add and manual refresh // Delicate API
             CoroutineScope(Dispatchers.Main).launch {
-                updateBitcoinExplorerAppWidget(context, appWidgetManager, appWidgetId)
+                updateQuotesTransparentAppWidget(context, appWidgetManager, appWidgetId)
             }
         }
     }
@@ -47,7 +39,7 @@ class BitcoinExplorerAppWidget : AppWidgetProvider() {
 }
 
 @RequiresApi(Build.VERSION_CODES.O)
-internal suspend fun updateBitcoinExplorerAppWidget(
+internal suspend fun updateQuotesTransparentAppWidget(
     context: Context,
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int
@@ -61,18 +53,14 @@ internal suspend fun updateBitcoinExplorerAppWidget(
     val quoteDate = response.date
 
     // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.bitcoinexplorer_app_widget)
+    val views = RemoteViews(context.packageName, R.layout.quotes_transparent_app_widget)
 
     // Set values to widget
-    views.setTextViewText(R.id.textSnapshotTime, quoteText)
+    views.setTextViewText(R.id.quoteTextTransparent, quoteText)
+    views.setTextViewText(R.id.quoteSpeakerTransparent, "$quoteSpeaker : $quoteDate")
 
-    // Set last update date time
-    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-    val current = LocalDateTime.now().format(formatter)
-    views.setTextViewText(R.id.textLastUpdated, current)
-
-    // refresh button
-    val intentUpdate = Intent(context, BitcoinExplorerAppWidget::class.java)
+    // refresh action
+    val intentUpdate = Intent(context, QuotesTransparentAppWidget::class.java)
     intentUpdate.action = AppWidgetManager.ACTION_APPWIDGET_UPDATE
 
     val idArray = intArrayOf(appWidgetId)
@@ -86,7 +74,8 @@ internal suspend fun updateBitcoinExplorerAppWidget(
         PendingIntent.FLAG_IMMUTABLE
     )
 
-    views.setOnClickPendingIntent(R.id.buttonRefresh, pendingUpdate)
+    // Set the object able to refresh and get a new random quote.
+    views.setOnClickPendingIntent(R.id.quoteTransparentWidget, pendingUpdate)
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
