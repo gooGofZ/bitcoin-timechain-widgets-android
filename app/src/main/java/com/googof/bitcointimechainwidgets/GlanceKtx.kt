306f1799742a5652a3338f1bc2ef5400db0f67b4
gooGofZ <darksmartz@gmail.com>
2022-12-29 09:01:03 +0700

    - Reduce white padding on the mempool widget for better UI resizing. - Change font size on the widgets

--- app/src/main/java/com/googof/bitcointimechainwidgets/GlanceKtx.kt
+++ app/src/main/java/com/googof/bitcointimechainwidgets/GlanceKtx.kt
@@ -1,12 +1,9 @@
 package com.googof.bitcointimechainwidgets
 
-import android.content.res.Resources
 import android.os.Build
-import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 import androidx.glance.GlanceModifier
-import androidx.glance.LocalContext
 import androidx.glance.appwidget.appWidgetBackground
 import androidx.glance.appwidget.cornerRadius
 import androidx.glance.background
@@ -52,7 +49,7 @@ fun AppWidgetColumn(
 @Composable
 fun appWidgetBackgroundModifier() = GlanceModifier
     .fillMaxSize()
-    .padding(16.dp)
+    .padding(10.dp)
     .appWidgetBackground()
     .background(GlanceTheme.colors.background)
     .appWidgetBackgroundCornerRadius()
